73cb984bff383ba588b2244b8ed29ea2efeb50fa
João Paulo Pizani Flor <joaopizani@gmail.com>
2013-10-07 01:00:51 +0200

    ALU on the way

--- circuits/circuit-01/lava/ALU.hs
+++ circuits/circuit-01/lava/ALU.hs
@@ -25,6 +25,7 @@ fullAdder (cin, (a, b)) = (s, cout)
       (s, c2)  = halfAdder (ab, cin)
       cout     = or2 (c1, c2)
 
+
 verifyFullAdder = simulateSeq fullAdder input
     where
       input = [ (low,  (low,  low))
@@ -45,3 +46,27 @@ testRippleCarryAdder = simulateSeq rippleCarryAdder input
     where
       input = [[(low,low), (low,low), (low,low), (low,low), (low,low), (low,low), (low,low), (low,low)
               , (low,low), (low,low), (low,low), (low,low), (low,low), (low,low), (low,low), (low,low) ]]
+
+
+andLifted :: [(Signal Bool, Signal Bool)] -> [Signal Bool]
+andLifted = map and2
+
+
+increment :: [Signal Bool] -> [Signal Bool]
+increment a = s
+    where (s, _) = row fullAdder (high, zip a (repeat high))
+
+
+alu :: ([Signal Bool], [Signal Bool], Signal Bool, Signal Bool, Signal Bool, Signal Bool, Signal Bool, Signal Bool)
+      -> ([Signal Bool], Signal Bool, Signal Bool)
+alu (x, y, zx, nx, zy, ny, f, no) = (out', zr, ng)
+    where
+      zr   = undefined  -- map or2 (map equalBool out' (repeat low))
+      ng   = undefined
+      out' = ifBool no (map inv out, out)
+      out  = ifBool f (rippleCarryAdder x'' y'', andLifted x'' y'')
+      x'   = ifBool zx (repeat low, x)
+      x''  = ifBool nx (inv x', x')
+      y'   = ifBool zy (repeat low, y)
+      y''  = ifBool ny (inv y', y')
+
