98b5b9fd28fac2e77395b2531aedeb8c0de90212
João Paulo Pizani Flor <joaopizani@gmail.com>
2013-10-15 18:42:55 +0200

    included test case outputs for lava Register

--- circuits/circuit-02/lava/Register.hs
+++ circuits/circuit-02/lava/Register.hs
@@ -9,7 +9,37 @@ reg (input, load) = out
     dff = mux (load, (out, input))
     out = delay low dff
 
+testReg1 :: [Signal Bool]
+testReg1 = simulateSeq reg inputs
+    where
+        inputs =
+            [ (low, high)
+            , (high, low)
+            , (high, low)
+            , (high, high)
+            , (low, low)
+            ]
+{-
+        outputs = [low, low, low, low, high]
+-}
+
+
 
 regN :: Int -> ([Signal Bool], Signal Bool) -> [Signal Bool]
 regN n (input, load) = map reg $ zip input (replicate n load)
 
+testRegN4 :: [[Signal Bool]]
+testRegN4 = simulateSeq (regN 4) inputs
+    where
+        allLow  = replicate 4 low
+        allHigh = replicate 4 high
+        inputs =
+            [ (allLow, high)
+            , (allHigh, low)
+            , (allHigh, low)
+            , (allHigh, high)
+            , (allLow, low)
+            ]
+{-
+        outputs = [allLow, allLow, allLow, allLow, allHigh]
+-}
