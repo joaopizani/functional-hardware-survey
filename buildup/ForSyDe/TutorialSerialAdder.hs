60bae3340af7686a095ac5fc44ff2658bbb0057e
João Paulo Pizani Flor <joaopizani@gmail.com>
2013-10-16 11:47:02 +0200

    finished ForSyDe serial full adder

--- buildup/ForSyDe/TutorialSerialAdder.hs
+++ buildup/ForSyDe/TutorialSerialAdder.hs
@@ -1,4 +1,49 @@
+{-# LANGUAGE TemplateHaskell #-}
 module TutorialSerialAdder where
 
 import ForSyDe
+
+
+faNextState :: ProcFun (Bit -> (Bit, Bit) -> Bit)
+faNextState = $(newProcFun [d| faNextState :: Bit -> (Bit, Bit) -> Bit
+                               faNextState st input =
+                                   if st == L then
+                                       case input of
+                                        (H, H) -> H
+                                        _      -> L
+                                    else
+                                        case input of
+                                            (L, L) -> L
+                                            _      -> H
+                           |])
+
+faOut :: ProcFun (Bit -> (Bit, Bit) -> Bit)
+faOut =
+    $(newProcFun
+        [d|
+            faOut :: Bit -> (Bit, Bit) -> Bit
+            faOut st input =
+                if st == L then
+                    case input of
+                        (L, L) -> L
+                        (L, H) -> H
+                        (H, L) -> H
+                        (H, H) -> L
+                else
+                    case input of
+                        (L, L) -> H
+                        (L, H) -> L
+                        (H, L) -> L
+                        (H, H) -> H
+        |]
+    )
+
+faProc :: Signal (Bit, Bit) -> Signal Bit
+faProc = mealySY "faProc" faNextState faOut L
+
+faSysDef :: SysDef (Signal (Bit, Bit) -> Signal Bit)
+faSysDef = newSysDef faProc "fa" ["a", "b"] ["sum"]
+
+faSim :: [(Bit, Bit)] -> [Bit]
+faSim = simulate faSysDef
 
