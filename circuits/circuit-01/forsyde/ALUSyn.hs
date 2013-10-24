63ba68d87493321c9d03a7d74fbc1154f1007664
João Paulo Pizani Flor <joaopizani@gmail.com>
2013-10-24 22:39:40 +0200

    ALU synthesizable with ForSyDe but cannot use complement function from Data.Bits

--- circuits/circuit-01/forsyde/ALUSyn.hs
+++ circuits/circuit-01/forsyde/ALUSyn.hs
@@ -2,62 +2,46 @@
 module ALUSyn where
 
 import ForSyDe
+import ForSyDe.Bit (bitToBool, boolToBit)
 import Data.Bits
 import Data.Int (Int16)
-
-import Language.Haskell.TH.Lift (deriveLift1)
-import Data.Generics (Data, Typeable)
 
 
 -- | The type of the operands AND output of the ALU. Must be a fixed-size integer
 type WordType = Int16
 
--- | Enumeration of possible operations that the ALU can perform
-data ALUOp = ALUSum | ALUAnd
-    deriving (Typeable, Data, Show)
+-- | Operation type. Low means Sum, High means bitwise And
+type ALUOp = Bit
 
-$(deriveLift1 ''ALUOp)
-
--- | Control bits of the ALU: zero X, negate X, zero Y, negate Y, op, negate out
+-- | Control bits of the ALU: zero X, negate X, zero Y, negate Y, (sum=0, and=1), negate out
 type ALUControl = (Bit, Bit, Bit, Bit, ALUOp, Bit)
 
 -- | Output flags: whether out is zero, whether out is negative
 type ALUFlags = (Bit, Bit)
 
 
--- Shorter synonyms, will be used often. Also, we could define here GLOBALLY
--- that signals should be active-low (if we wanted to)
-b2bo :: Bit -> Bool
-b2bo = bitToBool
-
-bo2b :: Bool -> Bit
-bo2b = boolToBit
-
 
 zProc :: ProcId -> Signal Bit -> Signal WordType -> Signal WordType
-zProc name = zipWithSY name zeroFunc
-    where zeroFunc = $(newProcFun [d| zeroFunc :: Bit -> WordType -> WordType
-                                      zeroFunc z w = if b2bo z then 0 else w |])
+zProc name = zipWithSY name $(newProcFun [d| f :: Bit -> WordType -> WordType
+                                             f z w = if z == H then 0 else w |])
 
+-- complement doesn't work, magic number 42 instead. Investigate why complement is out of scope
 nProc :: ProcId -> Signal Bit -> Signal WordType -> Signal WordType
-nProc name = zipWithSY name negFunc
-    where negFunc = $(newProcFun [d| negFunc :: Bit -> WordType -> WordType
-                                     negFunc n w = if b2bo n then complement w else w |])
+nProc name = zipWithSY name $(newProcFun [d| f :: Bit -> WordType -> WordType
+                                             f n w = if n == H then 42 else w |])
 
 compProc :: Signal ALUOp -> Signal WordType -> Signal WordType -> Signal WordType
-compProc = zipWith3SY "compProc" compFunc
-    where compFunc = $(newProcFun [d| compFunc :: ALUOp -> WordType -> WordType -> WordType
-                                      compFunc o x y = case o of
-                                                           ALUSum -> x + y
-                                                           ALUAnd -> x .&. y |])
+compProc = zipWith3SY "compProc" $(newProcFun [d| f :: ALUOp -> WordType -> WordType -> WordType
+                                                  f o x y = if o == H then x .&. y else x + y |])
 
 tzProc :: Signal WordType -> Signal Bit
-tzProc = mapSY "tzProc" $(newProcFun [d| tz :: WordType -> Bit
-                                         tz w = bo2b (w == 0) |])
+tzProc = mapSY "tzProc" $(newProcFun [d| f :: WordType -> Bit
+                                         f w = if w == 0 then H else L |])
 
 tnProc :: Signal WordType -> Signal Bit
-tnProc = mapSY "tnProc" $(newProcFun [d| tn :: WordType -> Bit
-                                         tn w = bo2b (w < 0) |])
+tnProc = mapSY "tnProc" $(newProcFun [d| f :: WordType -> Bit
+                                         f w = if w < 0 then H else L |])
+
 
 aluProc :: Signal ALUControl -> Signal WordType -> Signal WordType -> Signal (WordType, ALUFlags)
 aluProc c x y = zipSY "aluProc" out flags
