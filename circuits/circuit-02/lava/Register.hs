3e78e7f63a7bccab154886b0d1c71107b083bb6b
João Paulo Pizani Flor <joaopizani@gmail.com>
2013-10-28 23:52:29 +0100

    64-rows memory also working OK, all hail Haskell

--- circuits/circuit-02/lava/Register.hs
+++ circuits/circuit-02/lava/Register.hs
@@ -148,3 +148,10 @@ ram16Rows n (input, addr, load) = mux16WordN n (addr, registers)
         registers = map memLine (decode4To16 addr)
 
 
+ram64Rows :: Int -> ([SB], (SB,SB,SB,SB,SB,SB), SB) -> [SB]
+ram64Rows n (input, addr, load) = mux64WordN n (addr, registers)
+    where
+        memLine sel = regN n (input, sel <&> load)
+        registers = map memLine (decode6To64 addr)
+
+
