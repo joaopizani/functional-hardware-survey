345c031e005b29fe351bdc0c7e07b7f89d6d3b9a
João Paulo Pizani Flor <joaopizani@gmail.com>
2013-11-06 22:22:52 +0100

    forsyde muxes, with mux4 generating VHDL but a problem regarding process name management in mux16

--- circuits/circuit-02/forsyde/Register.hs
+++ circuits/circuit-02/forsyde/Register.hs
@@ -3,29 +3,58 @@ module Register where
 
 import ForSyDe
 
-import Prelude hiding ((++))
-import Prelude as P
-import Data.Param.FSVec
+import Data.Param.FSVec hiding ((++))
+import qualified Data.Param.FSVec as V
 
-import Data.TypeLevel.Num
+import Data.TypeLevel.Num hiding ((==))
 import Data.Int (Int16)
 
 
 type WordType = Int16
 
 
+-- BIG disadvantage of ForSyDe: name managing by hand
+
 reg :: String -> Signal WordType -> Signal Bit -> Signal WordType
 reg name input load = out
     where
         out = delaySY name (0 :: WordType) dff
-        dff = mux (name P.++ "M") load out input
+        dff = mux (name ++ ":M") load out input
 
+
+-- In the mux circuits, the selection bit vectors have the least-significant bit on the LEFT.
 
 mux :: ProcId -> Signal Bit -> Signal WordType -> Signal WordType -> Signal WordType
 mux l = zipWith3SY l $(newProcFun [d| f :: Bit -> WordType -> WordType -> WordType
-                                      f s x y = if s P.== L then x else y |])
+                                      f s x y = if s == L then x else y |])
 
-mux64 :: Signal (Bit,Bit,Bit,Bit,Bit,Bit) -> Signal (FSVec D64 WordType) -> Signal WordType
+mux4 :: ProcId -> Signal (FSVec D2 Bit) -> Signal (FSVec D4 WordType) -> Signal WordType
+mux4 l ss is = mux (l ++ ":m1")  (sv ! d1) m00 m01
+    where
+        sv = unzipxSY (l ++ ":unzipSel") ss
+        iv = unzipxSY (l ++ ":unzipInp") is
+        m00 = mux (l ++ ":m00") (sv ! d0) (iv ! d0) (iv ! d1)
+        m01 = mux (l ++ ":m01") (sv ! d0) (iv ! d2) (iv ! d3)
+
+mux4SysDef :: SysDef (Signal (FSVec D2 Bit) -> Signal (FSVec D4 WordType) -> Signal WordType)
+mux4SysDef = newSysDef (mux4 "muxA") "mux4Sys" ["sel", "inputs"] ["out"]
+
+
+mux16 :: ProcId -> Signal (FSVec D4 Bit) -> Signal (FSVec D16 WordType) -> Signal WordType
+mux16 l ss is = mux4 (l ++ ":m1") (sv ! d1) $ zipxSY (l ++ ":zipMs") (m00 +> m01 +> m02 +> m03 +> empty)
+    where
+        sv  = V.map (zipxSY (l ++ ":rezipSel")) $ group d2 $ unzipxSY (l ++ ":unzipSel") ss
+        iv  = V.map (zipxSY (l ++ ":rezipInp")) $ group d4 $ unzipxSY (l ++ ":unzipInp") is
+        m00 = mux4 (l ++ ":m00") (sv ! d0) (iv ! d0)
+        m01 = mux4 (l ++ ":m01") (sv ! d0) (iv ! d1)
+        m02 = mux4 (l ++ ":m02") (sv ! d0) (iv ! d2)
+        m03 = mux4 (l ++ ":m03") (sv ! d0) (iv ! d3)
+
+mux16SysDef :: SysDef (Signal (FSVec D4 Bit) -> Signal (FSVec D16 WordType) -> Signal WordType)
+mux16SysDef = newSysDef (mux16 "muxA") "mux16Sys" ["sel", "inputs"] ["out"]
+
+
+mux64 :: Signal (FSVec D6 Bit) -> Signal (FSVec D64 WordType) -> Signal WordType
 mux64 = undefined
 
 
