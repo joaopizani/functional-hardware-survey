6713832bd63e867511fb70f9338c3dd1b2cc01c4
João Paulo Pizani Flor <joaopizani@gmail.com>
2014-02-08 15:50:25 +0100

    added EDSL example, chosen EDSLs

--- presentation/code/parsec-example.hs
+++ presentation/code/parsec-example.hs
@@ -1,6 +1,6 @@
 dead, alive :: Parser Bool
 dead  = fmap (const False) (char '.')
-alive = fmap (const True) (char 'O')
+alive = fmap (const True) (char '*')
 
 line :: Parser [Bool]
 line  = many1 (dead <|> alive)
@@ -9,6 +9,6 @@ board :: Parser [[Bool]]
 board = line `endBy1` newline
 
 parseBoardFromFile :: FilePath -> IO [[Bool]]
-parseBoardFromFile filename =
-    do  result <- parseFromFile board filename
-        return $ either (error . show) id result
+parseBoardFromFile filename = do
+    result <- parseFromFile board filename
+    return $ either (error . show) id result
