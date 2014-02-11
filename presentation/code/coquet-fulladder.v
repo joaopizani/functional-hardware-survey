b6591025cadd4de29a03f588fef6b01730081392
João Paulo Pizani Flor <joaopizani@gmail.com>
2014-02-11 18:29:13 +0100

    Coquet intro, summarized some code.

--- presentation/code/coquet-fulladder.v
+++ presentation/code/coquet-fulladder.v
@@ -1,5 +1,9 @@
+Definition HADD a b s c: circuit ([:a]+[:b]) ([:s]+[:c]) :=
+    Fork2 ([:a] + [:b])  |>  (XOR a b s  &  AND a b c).
+
+
 Program Definition FADD a b cin sum cout :
-    circuit  ([:cin] + ([:a] + [:b])) ([:sum] + [:cout]) :=
+    circuit ([:cin] + ([:a] + [:b])) ([:sum] + [:cout]) :=
 
    (ONE [: cin]  &  HADD a b "s" "co1")
 |> Rewire (*  (a, (b,c)) => ((a,b), c)  *)
--- presentation/code/coquet-halfadder.v
+++
@@ -1,3 +0,0 @@
-Definition HADD a b s c: circuit ([:a] + [:b]) ([:s] + [:c]) :=
-       Fork2 ([:a] + [:b])
-    |> (XOR a b s  &  AND a b c).
